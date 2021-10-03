8b4584567588788da4c87e028a2be722f2577d6f
RAJNEESH KHARE <rkhare@gammastack.com>
2021-10-03 21:50:01 +0530

    initial commit

---
+++ translate.php
@@ -0,0 +1,6 @@
+<?php
+    $apiConfig = 'config\api-configuration.json';
+    $translationConfig = 'config\api-configuration.json';
+    
+    var_dump(file_get_contents($apiConfig));
+    var_dump(file_get_contents($translationConfig));
